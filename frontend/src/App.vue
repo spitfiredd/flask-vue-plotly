ea7ff841866b809f336fab97a436ef106892ae9a
Daniel Donovan <spitfiredd@gmail.com>
2019-03-09 20:31:46 -0500

    with great power comes great responsibility

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HelloPlotly />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import HelloPlotly from "./components/HelloPlotly.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloPlotly
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/HelloPlotly.vue
@@ -0,0 +1,28 @@
+<template>
+  <vue-plotly :data="data" :layout="layout" :options="options"/>
+</template>
+
+
+<script>
+import VuePlotly from '@statnett/vue-plotly'
+
+export default {
+  components: {
+    VuePlotly
+  },
+  data: function () {
+    return {
+      data: [{
+        x: [1, 2, 3, 4],
+        y: [10, 11, 12, 13],
+        mode: 'markers',
+        marker: {
+          size: [40, 60, 80, 100]
+        }
+      }],
+      layout: {title: 'Marker Size', showlegend: false, height: 600, width: 1200},
+      options: {}
+    }
+  }
+}
+</script>
